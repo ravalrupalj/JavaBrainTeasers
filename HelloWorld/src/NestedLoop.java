11a57084711a941ed7d355194ca6f7869f1f05dd
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-08-26 21:34:58 -0400

    Java_Loop_Practice

--- HelloWorld/src/NestedLoop.java
+++ HelloWorld/src/NestedLoop.java
@@ -2,9 +2,9 @@
 public class NestedLoop {
 
 	public static void main(String[] args) {
-		for (int j=1;j<=4;j++) { 
-			for (int i=1;i<=4;i++) {
-				System.out.print("* ");
+		for (int j=1;j<=6;j++) { 
+			for (int i=1;i<=j;i++) {
+				System.out.print(i+" ");
 			}
 			System.out.println("");
 			
