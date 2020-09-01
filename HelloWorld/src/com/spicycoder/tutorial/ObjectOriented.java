d2d36a0c001f3286f26521b0eed5b4ba49cbcd86
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-08-31 21:56:44 -0400

    OOPS_Java

---
+++ HelloWorld/src/com/spicycoder/tutorial/ObjectOriented.java
@@ -0,0 +1,34 @@
+package com.spicycoder.tutorial;
+
+public class ObjectOriented {
+	public static void main(String[] args) {
+		AirCraft details=new AirCraft();
+		details.passangers=5;
+		details.cruiseSpeed=140;
+		details.fuelCapacity=56.2;
+		details.fuelBurnRate=9.5;
+		AirCraft techni=new AirCraft();
+		techni.passangers=4;
+		techni.cruiseSpeed=130;
+		techni.fuelCapacity=45.2;
+		techni.fuelBurnRate=4.5;
+		System.out.println("details of AirCraft: "+ details.passangers);
+		System.out.println("\ndetails of AirCraft: "+ techni.fuelBurnRate);
+		double average =details.cruiseSpeed/details.passangers;
+		double average2=techni.cruiseSpeed/techni.passangers;
+		System.out.println("\n" +average);
+		System.out.println("\n" +average2);
+		details.perform();
+		System.out.println(details.total);
+	}
+}
+class AirCraft{
+	int passangers;
+	int cruiseSpeed;
+	int total;
+	float fuelCapacity;
+	float fuelBurnRate;
+	public void perform() {
+		total=passangers+cruiseSpeed;
+	}
+}
