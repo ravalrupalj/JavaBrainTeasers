d84dcfd9fb95d322a19f84f283d2a9c808b25036
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-09-24 21:45:58 -0400

    Java Practise

---
+++ HelloWorld/src/Edabit/Addition.java
@@ -0,0 +1,12 @@
+package Edabit;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		System.out.println(add(-3));
+	}
+	public static int add(int a) {
+		int next=a+1;
+		return next;
+	}
+}
