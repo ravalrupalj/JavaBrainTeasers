5c12175601b39c8834409960ccf1989f94e95251
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-09-25 22:04:58 -0400

    Java Practise

---
+++ HelloWorld/src/Edabit/Challenge.java
@@ -0,0 +1,12 @@
+package Edabit;
+
+public class Challenge {
+
+	public static void main(String[] args) {
+		System.out.println(howManySeconds(10));
+	}
+	public static int howManySeconds(int hours) {
+		int seconds=hours*60*60;
+		return seconds;
+	}
+}
---
+++ HelloWorld/src/Edabit/NextEdge.java
@@ -0,0 +1,12 @@
+package Edabit;
+
+public class NextEdge {
+
+	public static void main(String[] args) {
+		System.out.println(nextEdge(5,7));
+	}
+	public static int nextEdge(int a, int b) {
+		int value=(a+b)-1;
+		return value;
+	}
+}
