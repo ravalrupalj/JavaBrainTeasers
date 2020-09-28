b5b7fced10e263bc43f5fe5f5a8eb499f48f8491
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-09-27 21:05:27 -0400

    Java_Practice

---
+++ HelloWorld/src/Edabit/EndingMatches.java
@@ -0,0 +1,14 @@
+package Edabit;
+
+public class EndingMatches {
+
+	public static void main(String[] args) {
+		System.out.println(checkEnding("hello","o"));
+		System.out.println(checkEnding("abc", "d"));
+	}
+	public static boolean checkEnding(String str1,String str2) {
+		int numberOFChar=str2.length();
+		String lastCharOFstr1=str1.substring(str1.length()-numberOFChar);
+		return lastCharOFstr1.equals(str2);
+	}
+}
---
+++ HelloWorld/src/Edabit/LeftDigit.java
@@ -0,0 +1,26 @@
+package Edabit;
+
+public class LeftDigit {
+	public static void main(String[] args) {
+		System.out.println(mostLeftDigit("TrAdE2W1n95!"));
+	}
+	public static int mostLeftDigit(String a){
+		for(int i=0;i<a.length();i++) {
+			if(((Character)a.charAt(i)).isDigit(a.charAt(i))) {
+				return Integer.parseInt(a.charAt(i)+"");
+			}
+		}
+		return 0;	
+	}
+	//or
+	public static int leftDigit(String str) {
+		for (char c : str.toCharArray()) {
+				if (Character.isDigit(c)) 
+					return Integer.parseInt(Character.toString(c));
+		}
+		return 0;		
+}
+
+
+}
+
