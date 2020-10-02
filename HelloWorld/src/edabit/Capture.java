7d57e5a8160445832fbc72af379a97bcb25a1d0d
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-10-01 21:01:07 -0400

    Java_Practice

--- HelloWorld/src/edabit/Capture.java
+++ HelloWorld/src/edabit/Capture.java
@@ -1,10 +1,14 @@
 package edabit;
 
+import java.util.Arrays;
+
 public class Capture {
 
 	public static void main(String[] args) {
 		String[] input = new String[]{"A8","E8"};
 		System.out.println(canCapture(input));
+		int[] output=hashPlusCount("###+");
+		System.out.println(Arrays.toString(output));
 	}
 	public static boolean canCapture(String[] rooks) {
 		if ((rooks[0].charAt(0)==rooks[1].charAt(0)) || (rooks[0].charAt(1)==rooks[1].charAt(1))){
@@ -13,6 +17,21 @@ public class Capture {
 		else {
 			return false;
 		}
+	}
+	public static int[] hashPlusCount(String s) {
+		int count1=0;
+		int count2=0;
+		for (int i=0;i<s.length();i++) {
+			if(s.charAt(i)=='#') {
+				count1=count1+1;
+			}
+			if(s.charAt(i)=='+') {
+				count2=count2+1;
+			}
+		}
+		int[] obj=new int[] {count1 ,count2};
+		return obj;
+	
 	}
 
 }
