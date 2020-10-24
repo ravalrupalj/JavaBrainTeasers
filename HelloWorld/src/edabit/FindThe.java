a7ef15fb9b6e8a9ff86d2898df9adba5d7c89f13
ravalrupalj <63676082+ravalrupalj@users.noreply.github.com>
2020-10-23 22:33:04 -0400

    java_Practice

--- HelloWorld/src/edabit/FindThe.java
+++ HelloWorld/src/edabit/FindThe.java
@@ -57,7 +57,19 @@ public class FindThe {
 		System.out.println(String.join("",str));
 		return count;
 	}
-	public static int getBudgets(String s) {
+	public static String fizzBuzz(int n) {
+		if (n%3==0 & n%5==0) {
+			return "FizzBuzz";
+		}
+		else if (n%5==0) {
+			return "Buzz";
+		}
+		else if (n%3==0) {
+			return "Fizz";
+		}
+		else {
+			return n+"";
+		}
 		
 	}
 
